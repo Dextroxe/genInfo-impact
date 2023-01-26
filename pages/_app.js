5446f6da241be0a7fda7cba98689dfab9fe7f1bb
dashmat <dashmathembram99@gmail.com>
2023-01-26 17:56:05 +0530

    added dumy footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-// import Footer from '../components/Footer'
+import Footer from '../components/Footer'
 import Banner from '../components/Banner'
 import NavBar from './../components/NavBar'
 import { NextUIProvider } from '@nextui-org/react';
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
             <Component {...pageProps} />
           </Layout>
         </NextUIProvider>
-        {/* <Footer></Footer> */}
+        <Footer></Footer>
       </>
     </SSRProvider>
   )
